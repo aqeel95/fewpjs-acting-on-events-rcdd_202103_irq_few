aea86ceb8528568519b66affa19dafa69ad818ac
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-05-16 13:25:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,15 +32,27 @@ document.addEventListener("keydown", function(e) {
 
 
 function moveDodgerRight() {
+<<<<<<< HEAD
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
    
     if (left > 0) {
       dodger.style.left = `${left + 1}px`;
+=======
+    let rightNumbers = dodger.style.right.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+   
+    if (right > 0) {
+      dodger.style.right = `${left + 1}px`;
+>>>>>>> 90666538b5e38d4e05ce45f9e87c133b239e111f
     }
   }
   document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
+<<<<<<< HEAD
         moveDodgerRight();
+=======
+      moveDodgerRight();
+>>>>>>> 90666538b5e38d4e05ce45f9e87c133b239e111f
     }
   });
--- test/indexTest.js
+++ test/indexTest.js
@@ -16,20 +16,16 @@ describe('moveDodgerLeft()', () => {
   })
 })
 
-describe('moveDodgerRight', () => {
-  beforeEach(() => {
-    dodger = document.getElementById('dodger')
-  })
-
-  it('moves the DODGER to the right', () => {
-    var left = dodger.style.left
-    left = parseInt(left)
-
-    moveDodgerRight()
-
-    var newPosition = dodger.style.left
-    newPosition = parseInt(newPosition)
-
-    expect(newPosition).to.be.above(left)
-  })
-})
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
